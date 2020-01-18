11f48107f940babf89e40250baeab2381ea4e975
leessosso <leessosso@gmail.com>
2020-01-18 15:49:25 +0900

    Season Display

--- seasons/src/SeasonDisplay.js
+++ seasons/src/SeasonDisplay.js
@@ -1,7 +1,31 @@
 import React from "react";
 
-const SeasonDisplay = () => {
-  return <div>Season Display</div>;
+const getSeason = (lat, month) => {
+  if (month > 2 && month < 9) {
+    return lat > 0 ? "summer" : "winter";
+  } else {
+    return lat > 0 ? "winter" : "summer";
+  }
+};
+
+const SeasonDisplay = props => {
+  const season = getSeason(props.lat, new Date().getMonth());
+
+  alert(season);
+
+  return (
+    <div>
+      <h1>
+        {season === "winter" ? "Burr, it is chilly" : `Let's hit the beach`}
+      </h1>
+
+      {season === "winter" ? (
+        <i class="snowflake icon"></i>
+      ) : (
+        <i class="sun icon"></i>
+      )}
+    </div>
+  );
 };
 
 export default SeasonDisplay;
--- seasons/src/index.js
+++ seasons/src/index.js
@@ -1,9 +1,40 @@
 import React from "react";
 import ReactDOM from "react-dom";
+import SeasonDisplay from "./SeasonDisplay";
 
-const App = () => {
-  return <div>HI there!!</div>;
-};
+class App extends React.Component {
+  //THIS IS THE ONLY TIME we DO direct assignment
+  //to this.state
+  //babel이 알아서 생성자를 만들어준다.
+  state = { lat: null, errorMessage: "" };
+
+  componentDidMount() {
+    window.navigator.geolocation.getCurrentPosition(
+      position => {
+        console.log(position);
+        this.setState({ lat: position.coords.latitude });
+      },
+      err => {
+        console.log(err);
+        this.setState({ errorMessage: err.message });
+      }
+    );
+  }
+
+  componentDidUpdate() {
+    alert("my component was just updated - it rerendered!");
+  }
+
+  render() {
+    if (this.state.errorMessage && !this.state.lat) {
+      return <div>Error: {this.state.errorMessage}</div>;
+    }
+    if (!this.state.errorMessage && this.state.lat) {
+      return <SeasonDisplay lat={this.state.lat} />;
+    }
+    return <div>Loading!</div>;
+  }
+}
 
 ReactDOM.render(<App />, document.querySelector("#root"));
 
