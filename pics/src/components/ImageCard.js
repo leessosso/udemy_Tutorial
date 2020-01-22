0398b61cf7243a855b77e94188eca5f2be75f9c2
leessosso <leessosso@gmail.com>
2020-01-22 12:40:00 +0900

    add component  project

--- pics/src/components/ImageCard.js
+++ pics/src/components/ImageCard.js
@@ -1,12 +1,33 @@
 import React, { Component } from "react";
 
 class ImageCard extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = { spans: 0 };
+    this.imageRef = React.createRef();
+  }
+
+  componentDidMount() {
+    console.log(this.imageRef.current.clientHeight); //0이 찍힐건데 이미지 로딩이 아직 안돼서
+
+    this.imageRef.current.addEventListener("load", this.setSpans);
+  }
+
+  setSpans = () => {
+    const heigt = this.imageRef.current.clientHeight;
+
+    const spans = Math.ceil(heigt / 10);
+
+    this.setState({ spans });
+  };
+
   render() {
     const { description, urls } = this.props.image;
 
     return (
-      <div>
-        <img alt={description} src={urls.regular} />
+      <div style={{ gridRowEnd: `span ${this.state.spans}` }}>
+        <img ref={this.imageRef} alt={description} src={urls.regular} />
       </div>
     );
   }
